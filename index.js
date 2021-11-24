9a5398fcb992b10119925c8b3a4d13e283d5e7bd
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-24 15:04:41 -0300

    Aqui eu criei um switch que verifica a URL recebida pelo require, e, dependendo da mesma, executa um switch que retorna 2 possiveis resultadosm, sendo ele um JSON ou uma tag HTML escrita oi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 
-let server = http.createServer((req, res)  => { //criamos a variável server 
+let server = http.createServer((req, res) => { //criamos a variável server 
 
     console.log('URL', req.url) //Essa variável vai receber as solicitações do servidor, aqui é a URL
     console.log('METHOD', req.method) //Aqui é o método do servidor
@@ -10,16 +10,29 @@ let server = http.createServer((req, res)  => { //criamos a variável server
     switch (req.url) {
         case '/':
             res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
-            res.setHeader('Content-type', 'text/HTMl')//Retorna um header em HTML
-            res.end("<h1> ai...</h1>")            
+            res.setHeader('Content-Type', 'text/HTMl')//Retorna um header em HTML
+            res.end("<h1> ai...</h1>")
             break;
-    
-        default:
+
+        case '/test': //URl que o Req vai receber 
+
+
+            res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
+            res.setHeader('Content-Type', 'application/json')//especifíca o tipo de conteúdo, nesse caso JSON
+            res.end(JSON.stringify({ //Criação do JSON
+                users: [{
+                    name: "Reinaldo",
+                    email: "gamersolitavi4l@gmai.com",
+                    id: 1
+                }]
+            }))
+
             break;
+
     }
 });
 
-server.listen(3000, '127.0.0.1', ()=>{  //Inserimos a porta e o nosso IP local e uma função anonima
-console.log("servidor rodando") //Um console log para teste
+server.listen(3000, '127.0.0.1', () => {  //Inserimos a porta e o nosso IP local e uma função anonima
+    console.log("servidor rodando") //Um console log para teste
 
 })
