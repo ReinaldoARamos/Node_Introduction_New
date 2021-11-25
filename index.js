21e67964969308d512e01f1fa85698a55f95d4b8
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-25 12:01:34 -0300

    Exportando as rotas do index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express"); //Iniciando o framework Express, já que o mesmo já puxa HTTP
 let app = express(); //Essa variável retornar toda a aplicação e o conjunto de informaçõesn rodando intermnamente
+let routesIndex = require('./routes/index') 
+//Esse let faz com que o routeSIndex procure um arquivo index dentro de node_modules
+//porém temos que definir qual arquivo ele vai buscar, pois não necessáriamente é o
+//módulo index e sim um da pasta, por isso colocamos o diretório
 
 app.listen(3000, "127.0.0.1", () => {
   //Inserimos a porta e o nosso IP local e uma função anonima
