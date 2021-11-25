24cf1b298a52a0397d34b7cb73a549be01c5a4bf
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-25 15:24:58 -0300

    Configurando o index para poder usar as rotas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express"); //Iniciando o framework Express, já que o mesmo já puxa HTTP
-let app = express(); //Essa variável retornar toda a aplicação e o conjunto de informaçõesn rodando intermnamente
 let routesIndex = require('./routes/index') 
 let usersRouter = require('./routes/users')
+let app = express(); //Essa variável retornar toda a aplicação e o conjunto de informaçõesn rodando intermnamente
+
 //Esse let faz com que o routeSIndex procure um arquivo index dentro de node_modules
 //porém temos que definir qual arquivo ele vai buscar, pois não necessáriamente é o
 //módulo index e sim um da pasta, por isso colocamos o diretório
+app.use(routesIndex); //O método use serve para usar mais plugins noe empress
+app.use(usersRouter);
 
 app.listen(3000, "127.0.0.1", () => {
   //Inserimos a porta e o nosso IP local e uma função anonima
