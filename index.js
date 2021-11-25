bfaaabdd6f6d8ee9830c9a8d3294dde35a09d9e6
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-25 10:39:56 -0300

    COm o express nao e necessario usar o metodo JSON Stringy, entao apenas coloquei a resposta em JSON e ele ja reconhece

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.get('/' /*Antes das requisição e resposta passamos o parâmetro, que é a 
   
     res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
     res.setHeader('Content-Type', 'application/json')//especifíca o tipo de conteúdo, nesse caso JSON
-    res.end(JSON.stringify({ //Criação do JSON
+    res.json({ //Criação do JSON
         users: [{
             name: "Reinaldo",
             email: "gamersolitavi4l@gmai.com",
             id: 1
         }]
-    }))
+    })
     });
     //Foi removido o create server e substituído diretamente pelo metodo que vamos 
     //utilizar pela rota, nesse caso o GET
