08bbe3b5fae171544eba83932f3046fd6b769b74
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-26 15:22:39 -0300

    Fazendo a alteralçao das rotas para inserir o consign no projeto

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-let express = require('express')
-let routes = express.Router();
 
-routes.get(
+module.exports = (app)=>{
+
+app.get(
   "/" /*Antes das requisição e resposta passamos o parâmetro, que é a rota*/,
   (req, res) => {
     res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
@@ -11,7 +11,7 @@ routes.get(
   
 )
 ;
-module.exports = routes; 
+}; 
 //exportamos isso para que ele exporte o routes para quem tiver chamando
 //ou requerindo o index.js como um módulo
 
--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,46 @@
 
 
-module.exports = function(app){
 
-
-app.get(
-  "/user" /*Antes das requisição e resposta passamos o parâmetro, que é a rota*/,
-  (req, res) => {
-    res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
-    res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
-    res.json({
-      //Criação do JSON
-
-      users: [
-        {
-          name: "Reinaldo",
-          email: "gamersolitavi4l@gmai.com",
-          id: 1,
-        },
-      ],
-    });
-  }
-
+module.exports = (app)=>{
+  app.get(
+    "/user" /*Antes das requisição e resposta passamos o parâmetro, que é a rota*/,
+    (req, res) => {
+      res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
+      res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
+      res.json({
+        //Criação do JSON
   
-);
-
-app.get('/user/admin' ,  (req, res) => {
-  res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
-    res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
-    res.json({
-      //Criação do JSON
-
-      users: [
-        
-      ],
-    });
-  }
-
+        users: [
+          {
+            name: "Reinaldo",
+            email: "gamersolitavi4l@gmai.com",
+            id: 1,
+          },
+        ],
+      });
+    }
   
-
-);
     
-}; 
+  );
+  
+  app.get('/user/admin' ,  (req, res) => {
+    res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
+      res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
+      res.json({
+        //Criação do JSON
+  
+        users: [
+          
+        ],
+      });
+    }
+  
+    
+    
+  );
+      
+  
+
+}
 //Foi removido o create server e substituído diretamente pelo metodo que vamos
 //utilizar pela rota, nesse caso o GET
