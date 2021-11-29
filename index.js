98065e13544ee4e5defe61ec6d8f7fe5329556bb
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-29 16:57:28 -0300

    instalando o body-parser

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express"); //Iniciando o framework Express, já que o mesmo já puxa HTTP
 const consign = require("consign");
-let bodyParser = require("body-parse")
+let bodyParser = require("body-parser")
 let app = express(); //Essa variável retornar toda a aplicação e o conjunto de informaçõesn rodando intermnamente
 //Esse let faz com que o routeSIndex procure um arquivo index dentro de node_modules
 //porém temos que definir qual arquivo ele vai buscar, pois não necessáriamente é o
 //módulo index e sim um da pasta, por isso colocamos o diretório
-app.arguments(bodyParser.json());
+
+
+app.use(bodyParser.urlenconded({extended: false}));
+app.use(bodyParser.json());
 consign().include("routes").into(app);
 
 app.listen(3000, "127.0.0.1", () => {
