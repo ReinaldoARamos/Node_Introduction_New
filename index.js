851e1e4d858efba592d4580a7c0f796c77bafd5d
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-11-29 16:18:58 -0300

    Estamos começando a aplicar o body-parse: body-parse e  uma dependencia que pega os posts das rotas e converte em JSON, para isso funcionar temos que dar o require do body-parse e dar um app.use para ele ser usado no app, passando o Bodyparser com o metodo JSON justamente para ele fazer o parte de tudo que receber para JSON

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express"); //Iniciando o framework Express, já que o mesmo já puxa HTTP
 const consign = require("consign");
+let bodyParser = require("body-parse")
 let app = express(); //Essa variável retornar toda a aplicação e o conjunto de informaçõesn rodando intermnamente
 //Esse let faz com que o routeSIndex procure um arquivo index dentro de node_modules
 //porém temos que definir qual arquivo ele vai buscar, pois não necessáriamente é o
 //módulo index e sim um da pasta, por isso colocamos o diretório
-
+app.arguments(bodyParser.json());
 consign().include("routes").into(app);
 
 app.listen(3000, "127.0.0.1", () => {
