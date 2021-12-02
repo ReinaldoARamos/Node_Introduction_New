51c12b71ed420ada274cb742812f6d0af323dfe4
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-12-02 17:57:56 -0300

    CONCERTANDO ERROS E RECOMEÇANDO ESSA PARTE DO CODIGO

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ module.exports = (app) => {
   app.get(
     "/user" /*Antes das requisição e resposta passamos o parâmetro, que é a rota*/,
     (req, res) => {
+      
       res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
       res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
       res.json({
