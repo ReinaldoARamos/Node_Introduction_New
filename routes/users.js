55584287a984f75acd335c65a5af9235089da5bc
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2021-12-30 17:30:04 -0300

    Aqui eu criei uma rota Id que vai tratar de receber os usuarios retornados quando buscamos pelo Id, ele usa a funçao de retornar erro caso haja um e caso consiga encontrar o user, ele o retorna, e como estamos usando o findOne ele busca somente um usuario que se encaixa no parametro buscado

--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,62 @@
-let neDB = require('nedb') //require do banco de dados em javaScript
+let neDB = require("nedb"); //require do banco de dados em javaScript
 
 let db = new neDB({
-  filename: 'users.db',
-  autoload: true
-})
+  filename: "users.db",
+  autoload: true,
+});
 
 module.exports = (app) => {
-  let route = app.route('/user')
+  let route = app.route("/user");
   route.get((req, res) => {
-      
-        db.find({}).sort({nAME: 1}).exec((err, users) => {//exec vai executar os parametros do insert passado no post
-          if (err) {
-            app.utils.error.send(err, req, res);//carrega o modulo utils e chama o método send e seus parametros
-          }else{
-            
-            res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
-            res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
-            res.json({
-              //Criação do JSON
-      
-              users: users
-            });
-          }
-        })
+    db.find({})
+      .sort({ nAME: 1 })
+      .exec((err, users) => {
+        //exec vai executar os parametros do insert passado no post
+        if (err) {
+          app.utils.error.send(err, req, res); //carrega o modulo utils e chama o método send e seus parametros
+        } else {
+          res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
+          res.setHeader("Content-Type", "application/json"); //especifíca o tipo de conteúdo, nesse caso JSON
+          res.json({
+            //Criação do JSON
 
-   
-    }
-  );
+            users: users,
+          });
+        }
+      });
+  });
 
   route.post((req, res) => {
     //foi apagado o get e trocado pelo post
     // para que possamos simular um no Psotman
 
-    
-  
     db.insert(req.body, (err, users) => {
-      if(err) {
-        
+      if (err) {
         app.utils.error.send(err, req, res);
-      } else{
+      } else {
         res.status(200).json(users); //aqui ele recebe o codigo 200(sucesso) e inseri o json do user
       }
-    })
+    });
     //Aqui dentro usamos o método insertb para inserir dados no banco
-    //nele colocamos o objto json que estamos retornando e uma função, como a erro por exemplo, 
+    //nele colocamos o objto json que estamos retornando e uma função, como a erro por exemplo,
     //caso o método retorne erro, como o de cadastro ja existente
     //e claro, colocamos também os dados do usuário
+  });
 
-  
-  }); 
+  let routeId = app.route("/user/:id"); //Criei uma roto que vai receber os ids dos users
+
+  routeId.get((req, res) => {
+    //Estabeleci o método get, coloquei a requisisão e a resposta
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user); //aqui ele recebe o codigo 200(sucesso) e inseri o json do user
+      }
+    });
+    //aqui ele usa o método findOne para buscar o parametro selecionado
+    //no caso os ids
+  });
 };
 //Foi removido o create server e substituído diretamente pelo metodo que vamos
 //utilizar pela rota, nesse caso o GET
