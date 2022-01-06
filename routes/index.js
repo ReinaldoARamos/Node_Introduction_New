80d4caf6c1ec3a991a9bce8fffde9247ea7f6d9b
Reinaldo Ramos <analista2@grupomundialmix.com.br>
2022-01-06 09:15:45 -0300

    Adicionando mais documentaçao

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
       res.statusCode = 200; //Código de quando o usuário acessa cm sucesso o servidor
       res.setHeader("Content-Type", "text/HTMl"); //Retorna um header em HTML
       res.end("<h1> ai....</h1>");
+      //Aqui caso a rota sesa / ele vai criar um Header HTML e enviiar como resposta do req
     }
 
   )
--- routes/utils/error.js
+++ routes/utils/error.js
@@ -1,8 +1,10 @@
 module.exports = {
-    send: (err, req, res, code = 400) =>{
+    send: (err, req, res, code = 400) =>{ //passamos os parametros dentro do método send 
 
         console.log(`error: ${err}`);
-        res.status(code).json({
+        res.status(code).json({ 
+            //caso ocorra um erro ele retorna o status, no caso 400 comor esposta
+            //Do require em formato de JSON
             error:err 
             
             
